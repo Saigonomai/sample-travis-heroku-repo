8191bdf3d1e5da2d3c42e7af03c530aa5a91028b
Chentao Tang <chentao.tang@mail.utoronto.ca>
2019-05-09 19:47:42 -0400

    corrected functionality

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 /* server.js main file */
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -8,7 +9,7 @@ var PORT = process.env.PORT || 3001;
 app.get('/', (req, res) => {
   // eslint-disable-next-line no-console
   console.log('req = \n', req);
-  res.send('Test CI with Travis');
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const server = app.listen(PORT, () => {
