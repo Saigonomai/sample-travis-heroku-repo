1a579c2dae5df195f5d23e6a0bcf495fb8181d80
Chentao Tang <chentao.tang@mail.utoronto.ca>
2019-05-14 20:35:56 -0400

    added exit

--- tests/index-spec.js
+++ tests/index-spec.js
@@ -8,5 +8,6 @@ console.log("test file, server = \n", server);
 describe('test', () => {
     it('this simple test should return a string', () => {
         expect('Test CI with Travis').to.equal('Test CI with Travis');
+        process.exit();
     });
 });
