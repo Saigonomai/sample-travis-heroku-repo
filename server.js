b866d9b87d3d037b4143e0e458ddb8f5b9d09c9f
Chentao Tang <chentao.tang@mail.utoronto.ca>
2019-05-14 20:26:23 -0400

    git message

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 /* server.js main file */
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-var PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
   // eslint-disable-next-line no-console
   console.log('req = \n', req);
   res.send('Test CI with Travis');
-  // res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const server = app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('app running on port 3001');
+  console.log('app running on port = ', PORT);
 });
 
 // eslint-disable-next-line no-console
